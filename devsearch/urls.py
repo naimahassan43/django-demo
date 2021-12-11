7024aae5011fd970896ac88e37e9652336c8862f
Naima Hassan <naima.hassan434@gmail.com>
2021-12-11 23:34:21 +0000

    Views and URLs

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,19 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def projects(request):
-    return HttpResponse('Here are our products')
-
-def project(request, pk):
-    return HttpResponse('Here is Single product'+ ' ' +str(pk))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('home/', getHomePage),
-    path('projects/', projects, name='projects'),
-    path('project/<str:pk>/', project, name='project'),
-
-    # path('profiles/', admin.site.urls),
+    path('',include('projects.urls'))
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('Here are our products')
+
+def project(request, pk):
+    return HttpResponse('Here is Single product'+ ' ' +str(pk))
