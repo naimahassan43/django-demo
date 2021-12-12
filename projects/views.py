36ad13f10f3bf86d4bf877be68b55403e97fed01
Naima Hassan <naima.hassan434@gmail.com>
2021-12-12 00:34:53 +0000

    Templates and Template Inheritance

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def projects(request):
-    return HttpResponse('Here are our products')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('Here is Single product'+ ' ' +str(pk))
+    return render(request, 'projects/single-project.html')
